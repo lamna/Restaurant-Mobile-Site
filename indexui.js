966fac69b64b44c30e6dd33824a59b244830c608
Lamna Ravindran <lemnasachin@gmail.com>
2015-08-16 16:50:55 -0700

    Created mobile version - restaurant website

---
+++ indexui.js
@@ -0,0 +1,50 @@
+$(document).on('ready', function(){
+	function showLoad(){
+		$.mobile.loading( 'show', {
+			text: 'loading',
+			textVisible: true,
+			theme: 'z',
+			html: ""
+		});	
+	}
+	function stopLoad(){       
+		$.mobile.loading( 'hide', {
+			text: 'loading',
+			textVisible: false,
+			theme: 'z',
+			html: ""
+		});
+		$.mobile.changePage('#page2');
+    }
+    
+	$('#page2').on('click' ,function(){
+		showLoad();
+		stopLoad();
+	});
+	
+
+	
+	/*$(function() {
+		$( "#dialog" ).dialog();
+    });  */
+	
+	$('#login').click(function(){
+		$('#box').dialog({
+			title: 'Login',
+			width: 500,
+			height: 300,
+			Draggable: false,
+			resizable: false,
+			modal: true,
+			buttons: [
+				{
+					text: 'Close',
+					click: function(){
+						$(this).dialog('close');
+					}
+				}
+			]
+		});		
+	});	
+});	
+	
